ac24aa7c1c3f9b8cae61cdf41149ce580a4d37ee
manu1080 <manuelirazabal@hotmail.com>
2023-03-27 22:39:14 +0200

    Swagger example @property

--- apps/orchestration/src/dto/subscription.dto.ts
+++ apps/orchestration/src/dto/subscription.dto.ts
@@ -1,27 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class SubscriptionDto {
   @IsString()
   @IsOptional()
+  @ApiProperty()
   firstName: string;
 
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty()
   gender: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   dateOfBirth: string;
 
   @IsBoolean()
   @IsNotEmpty()
+  @ApiProperty()
   consent: boolean;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty()
   newsletterId: number;
 }
